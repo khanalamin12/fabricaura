dda259320c03484f58855d48caadddc875d1e97c
Khan Al-amin <khanalamin0092@gmail.com>
2026-06-20 15:47:55 +0600

    Update _shared.js

--- netlify/functions/_shared.js
+++ netlify/functions/_shared.js
@@ -1,9 +1,9 @@
 const CATEGORY_MAP = {
-  'drop-shoulder': { file: 'products.js', arrayVar: 'DROP_SHOULDER_PRODUCTS', min: 1001, max: 1999, codePrefix: 'DS' },
-  'pants':         { file: 'pants.js',    arrayVar: 'PANTS_PRODUCTS',         min: 2001, max: 2999, codePrefix: 'PT' },
-  'shirt':         { file: 'shirts.js',   arrayVar: 'SHIRTS_PRODUCTS',        min: 4001, max: 4999, codePrefix: 'SH' },
-  'tshirt':        { file: 'tshirts.js',  arrayVar: 'TSHIRTS_PRODUCTS',       min: 3001, max: 3999, codePrefix: 'TS' },
-  'watch':         { file: 'watches.js',  arrayVar: 'WATCHES_PRODUCTS',       min: 5001, max: 5999, codePrefix: 'WT' },
+  'drop-shoulder': { file: 'products.js', arrayVar: 'DROP_SHOULDER_PRODUCTS', min: 1001, max: 1999, codePrefix: 'DS', label: 'Drop-Shoulder Tee' },
+  'pants':         { file: 'pants.js',    arrayVar: 'PANTS_PRODUCTS',         min: 2001, max: 2999, codePrefix: 'PT', label: 'Pants' },
+  'shirt':         { file: 'shirts.js',   arrayVar: 'SHIRTS_PRODUCTS',        min: 4001, max: 4999, codePrefix: 'SH', label: 'Shirt' },
+  'tshirt':        { file: 'tshirts.js',  arrayVar: 'TSHIRTS_PRODUCTS',       min: 3001, max: 3999, codePrefix: 'TS', label: 'T-Shirt' },
+  'watch':         { file: 'watches.js',  arrayVar: 'WATCHES_PRODUCTS',       min: 5001, max: 5999, codePrefix: 'WT', label: 'Watch' },
 };
 
 module.exports = { CATEGORY_MAP };
